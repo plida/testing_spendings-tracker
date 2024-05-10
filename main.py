0d42fe915c3d36e305e41901d57b4467547d50c5
Plida <85080905+Plida@users.noreply.github.com>
2024-05-10 20:54:20 +0300

    changed imports in main

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 import datetime
 from tkinter import *
-import gui as Gui
-import db as Db
+import gui
+import db
 
 root = Tk()
-app = Gui.App(root)
+app = gui.App(root)
 
 
 class Terminal:
     def add_category(self, name):
         if name:
-            Db.Categories.add(name)
+            db.Categories.add(name)
             self.list_categories()
 
     def remove_category(self, name):
-        Db.Categories.remove(name)
+        db.Categories.remove(name)
         self.list_categories()
 
     @staticmethod
     def add_spending(data):
-        Db.Spendings.add(data)
+        db.Spendings.add(data)
 
     @staticmethod
     def remove_spending(uid):
-        Db.Spendings.remove(uid)
+        db.Spendings.remove(uid)
 
     @staticmethod
     def list_categories():
-        data = Db.Categories.get_all()
+        data = db.Categories.get_all()
         app.page2.fill_categories(data)
 
     @staticmethod
     def list_spendings():
-        data = Db.Spendings.get_all()
+        data = db.Spendings.get_all()
         app.page1.fill_spendings(data)
 
 
